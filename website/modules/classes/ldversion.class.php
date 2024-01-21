a9f37a161ad191c13fdc548b77f179977d2f5ccd
JeanFreitas <jean.sistemas@lar.ind.br>
2024-01-21 01:54:31 -0300

    teste

--- website/modules/classes/ldversion.class.php
+++ website/modules/classes/ldversion.class.php
@@ -8,26 +8,26 @@ if ( class_exists( "ldVersion" ) == false ) {
 	class ldVersion {
         public $version = NULL;
 		public function __construct(){}
-        
+
         public function setCurrentVersion($version)
         {
-            $this->version = $version;  
+            $this->version = $version;
         }
-        
+
         public function getCurrentVersion()
         {
-            return $this->version;  
+            return $this->version;
         }
-        
+
         public function getEncodeCurrentVersion()
         {
-            return "0x".strtoupper(dechex(crc32( $this->version )));  
+            return "0x".strtoupper(dechex(crc32( $this->version )));
         }
-        
+
         public function getMessageUpdateNotDefined()
-        {                                                                                
+        {
             if($_GET['updateKey'] == "show")
-                exit("Update key for Changelog.html: ". $this->getEncodeCurrentVersion());                                                                                                          
+                exit("Update key for Changelog.html: ". $this->getEncodeCurrentVersion());
             echo("<h1>".SITE_UNDER_MAINTENANCE."</h1><hr />");
             echo("<h3>".MESSAGE_TO_ADMIN."</h3>");
             echo(OLD_VERSION_EXPLICATION1."<br /><br />");
@@ -37,20 +37,21 @@ if ( class_exists( "ldVersion" ) == false ) {
             echo(OLD_VERSION_INSTALL_VERSION." ".$this->getCurrentVersion().".");
             exit();
         }
-        
+
         public function getMessageUpdateWrong()
-        {                     
+        {
             if($_GET['updateKey'] == "show")
-                exit("Update key for Changelog.html: ". $this->getEncodeCurrentVersion());                                                                                                          
+                exit("Update key for Changelog.html: ". $this->getEncodeCurrentVersion());
             echo("<h1>".SITE_UNDER_MAINTENANCE."</h1><hr />");
             echo("<h3>".MESSAGE_TO_ADMIN."</h3>");
             printf(NEW_VERSION_EXPLICATION1."<br /><br />", $this->getCurrentVersion());
             echo(STEPS_TO_REMOVE_MSG);
             exit();
         }
-        
+
         public function compareCurrentVersion()
         {
+            echo "Teste" . "</br>";
             if( defined("updateKeyChangelog") == false )
                 $this->getMessageUpdateNotDefined();
             if( constant("updateKeyChangelog") != $this->getEncodeCurrentVersion() )
